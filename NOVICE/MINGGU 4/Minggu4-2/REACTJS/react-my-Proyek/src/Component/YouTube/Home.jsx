41c3687de1a64e948a56bf2f36f463723cd61418
chamida12 <you@example.com>
2022-05-25 15:45:46 +0700

    Dynamic component menggunakan props

--- NOVICE/MINGGU 4/Minggu4-2/REACTJS/react-my-Proyek/src/Component/YouTube/Home.jsx
+++ NOVICE/MINGGU 4/Minggu4-2/REACTJS/react-my-Proyek/src/Component/YouTube/Home.jsx
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
-import YouTube from "./Component/YouTube/YouTube";
+import YouTubeCom from "./YouTube";
 
 class Home extends Component{
     render(){
      return (
         <div>
-            <p> Test </p>
-            <YouTube/>
-            <YouTube/>
-            <YouTube/>
+            <p> <h1> Youtube Ngaji Coding </h1> </p>
+            <YouTubeCom 
+             time="3.19" 
+             title=" Tutorial Reactjs Part 1"
+             desc="100rb x ditonton, 3 hari yang lalu"/>
+            <YouTubeCom 
+             time="4.10"
+             title=" Tutorial Reactjs Part 2"
+             desc="10rb x ditonton, 1 hari yang lalu"/>
+            <YouTubeCom 
+             time="7.30" 
+             title=" Tutorial Reactjs Part 3"
+             desc="20rb x ditonton, 8 jam yang lalu"/>  
+            <YouTubeCom 
+             time="9.03" 
+             title=" Tutorial Reactjs Part 4"
+             desc="500rb x ditonton, 10 hari yang lalu"/>
+            <YouTubeCom/>  
+           
+            
+      
 
         </div>
      
--- NOVICE/MINGGU 4/Minggu4-2/REACTJS/react-my-Proyek/src/Component/YouTube/YouTube.jsx
+++ NOVICE/MINGGU 4/Minggu4-2/REACTJS/react-my-Proyek/src/Component/YouTube/YouTube.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import './YouTube.css';
 
-const YouTube = () => {
+const YouTubeCom = (props) => {
   return (
        <div className="youtube-wrapper">
          <div className="img-thumb">
-           <img src="https://tabloidtekno.com/wp-content/uploads/2019/09/cara-membuat-cover-highlight-instagram.jpg"/>
-           <p className="time">7.12</p>
+           <img src="https://cdn.mos.cms.futurecdn.net/9QTpESGBXa32D29J77VR3d.jpg"/>
+           <p className="time">{props.time}</p>
          </div>
-           <p className="title">  title here</p>
-           <p className="desc"> desc here </p>
+           <p className="title"> {props.title}</p>
+           <p className="desc"> {props.desc} </p>
           
        </div>
 
   )
 }
-export default YouTube;
+YouTubeCom.defaultProps ={
+  time: '00.00',
+  title: ' Coding ',
+  desc: '1x ditonton, 2 hari yang lalu'
+}
+
+
+export default YouTubeCom;
